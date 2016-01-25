c054dfc476f286ac95c770bd8c4b2d04ab4015bd
Abigail Rolling and Brad London <arollingj@gmail.com>
2016-01-25 10:22:07 -0800

    Add functioning front end

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,28 @@ Vacation.prototype.placeName =function() {
 Vacation.prototype.timeDuration = function() {
   return this.duration + ' days in the ' + this.season.toLowerCase();
 }
+// END BUSINESS LOGIC
+
+$(document).ready(function() {
+  $("form#new-vacation").submit(function(event) {
+    event.preventDefault();
+    var inputtedCity = $("input#new-vacation-city").val();
+    var inputtedCountry = $("input#new-vacation-country").val();
+    var inputtedSeason = $("input#new-vacation-season").val();
+    var inputtedDuration = parseInt($("input#new-vacation-duration").val());
+    var inputtedHighlights = $("input#new-vacation-highlights").val();
+
+    var newVacation = new Vacation(inputtedCity, inputtedCountry, inputtedSeason, inputtedDuration, inputtedHighlights);
+    console.log(newVacation);
+    $("ul#places").append('<li><span class="place">' + newVacation.placeName() + '</span></li>');
+
+    $("input#new-vacation-city").val("");
+    $("input#new-vacation-country").val("");
+    $("input#new-vacation-season").val("");
+    $("input#new-vacation-duration").val("");
+    $("input#new-vacation-highlights").val("");
+
+
+  });
+
+}); // End document ready function
